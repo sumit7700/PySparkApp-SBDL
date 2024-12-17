3f75344f4efead04ed55d49ca3605601f68c9eb6
sumit7700 <38032909+sumit7700@users.noreply.github.com>
2024-12-17 11:54:59 -0500

    modified lib logger and logging included in  main file

--- cap_main.py
+++ cap_main.py
@@ -8,3 +8,13 @@ if __name__ =="__main__":
     if len(sys.argv) < 3:
         print("Usage  Capstone {local,qa,prod} {load_date} : Arguments are missing")
         sys.exit(-1)
+
+    job_run_env = sys.argv[1].upper()
+    load_date = sys.argv[2]
+
+    spark = utils.get_spark_session(job_run_env)
+    logger = log4J(spark)
+
+    logger.info("Finished creating Spark Session")
+
+
--- lib/logger.py
+++ lib/logger.py
@@ -1,10 +1,7 @@
 class log4J:
     def __init__(self,spark):
         log4j = spark._jvm.org.apache.log4j
-        root_class = "guru.learningjournal.spark.examples"
-        conf = spark.sparkContext.getConf()
-        app_name = conf.get("spark.app.name")
-        self.logger = log4j.LogManager.getLogger(root_class + "." + app_name)
+        self.logger = log4j.LogManager.getLogger("sbdl")
 
     def info(self,message):
         self.logger.info(message)
